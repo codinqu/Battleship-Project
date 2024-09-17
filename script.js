32af7a43ba0dca26398020205dcaca6d85edafc7
codinqu <Bianca.McKinley@mscoding.org>
2024-09-17 11:50:42 -0700

    comments added on css and js
    comments added for specification and clarification on styling and JS.

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+// setting Global variables
 const maxMines = 4;
 
 const rowSize = 3;
@@ -10,6 +11,8 @@ const gridContain = document.getElementById('gridBtns');
 const mines = createMines(maxMines, maxBattleSize);
 
 
+
+// declaring a function to get a random number
 function getRandomInt(max) {
 
     let ihateMath = Math.random() * max;
@@ -21,11 +24,15 @@ function getRandomInt(max) {
 }
 
 
-
+// declaring a function for the onClick event
+// giving my buttons there own unique ID
+// parsing the eventId 
 function onClick(event) {
     let eventId = event.target.id;
+    console.log(eventId);
 
     let btn = event.target;
+    console.log(btn);
 
     eventId = Number.parseInt(eventId);
 
@@ -33,15 +40,16 @@ function onClick(event) {
         console.log("you hit a mine");
 
         btn.classList.add('mines')
-    }
+    } // if this button id is = to the mine eventId then you hit a mine
     else{
         console.log("you live to see another day");
 
         btn.classList.add('clear');
-    }
+    } // if this button id is /= to this mine eventId then you did not hit a mine
 }
 
 
+// declaring a function to create my mines 
 function createMines(maxMines, maxBattleSize) {
     let mines = [];
 
@@ -51,13 +59,20 @@ function createMines(maxMines, maxBattleSize) {
 
        mines.push(mineId);
 
-    }
+    } // this is array will have four random numbers in it
+        // maxMines variable was set to 4, mineId is getting a random number from MBSize
+        // which is rowsize * rowsize
+    // push those random chosen numbers from MBSize and push for of those to the array
 
     return mines;
+    // return the array now with four random generated numbers that will signal the mines for that game
+    // always put return at the end of your function
 }
 
 
-
+// declaring a function and calling it this time
+// creating div, row, and buttons 
+// nested for loop to look through row size
 function main() {
 
     console.log("mines", mines);
@@ -69,22 +84,27 @@ function main() {
         let gridRow = document.createElement('div');
 
         gridRow.classList.add('row');
+        // adding a row to the div class
 
         for (let j = 0; j < rowSize; j++) {
             
             let makeBtns = document.createElement('button');
+            // creating a button(s)
 
             makeBtns.addEventListener("click",onClick);
+            // adding an event listener to buttons and calling the onClick function to run when buttons are clicked
 
             makeBtns.id = count
-            count++
+            count++ // the buttons id count goes up by one
 
             gridRow.appendChild(makeBtns);
             
-        }
+        } // appendChild to add btns at the end of row (parent?)
         
         gridContain.appendChild(gridRow);
+        // appendChild to add the grid row to the end of the grid container (parent?)
     }
 }
 
 main();
+// finally calling the function to run and execute the code inside of it
